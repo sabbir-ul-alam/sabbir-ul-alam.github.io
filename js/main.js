560a70b3af5349a9490c2e965bb7151d6ad47a3d
sabbirUlAlam <sabbir.ul.alam@tigerit.com>
2023-09-27 17:22:02 +0600

    Added carousel

--- js/main.js
+++ js/main.js
@@ -304,6 +304,10 @@
 		sliderMain();
 		stickyFunction();
 		owlCrouselFeatureSlide();
+		
+  // for every slide in carousel, copy the next slide's item in the slide.
+  // Do the same for the next, next item.
+
 	});
 
 
